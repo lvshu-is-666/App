70d6876bae55ffcd01c495a94ec18ec36df816a3
lvshu-is-666 <109424445+lvshu-is-666@users.noreply.github.com>
2025-02-19 22:51:14 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,4 @@
+
 // 防抖功能
 function debounce(fn, delay) {
     let timeout;
@@ -5,4 +6,26 @@ function debounce(fn, delay) {
         clearTimeout(timeout);
         timeout = setTimeout(() => fn.apply(this, args), delay);
     };
+}
+
+// 显示和隐藏置顶按钮
+let scrollBtn = document.getElementById('scroll-top-btn');
+const container = document.getElementById('app-detail'); // 替换为实际的滚动容器 ID
+
+if (container) {
+    container.addEventListener('scroll', () => {
+        if (container.scrollTop > 200) {
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    });
+} else {
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 200) {
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    });
 }
